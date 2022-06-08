9ffbd240e311648c7f0582ae5b314bb9c52898ba
Efrain Estrada <a325060@uach.mx>
2022-06-08 12:33:04 -0600

    heroku deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static(__dirname+'/dist/contact'));
+
+app.get('/*', (req, resp)=>{
+    resp.sendFile(__dirname+'/dist/contact/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
+
+
+
