445dc94169005975a07b49c8e336febec225e0c7
Vanessa-coder <idaliaflores788@gmail.com>
2022-06-09 10:31:59 -0600

    update camera

--- src/app/componentes/camera/camera.component.ts
+++ src/app/componentes/camera/camera.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.css']
 })
-export class CameraComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CameraComponent implements AfterViewInit{
+  WIDTH = 1280;
+  HEIGHT = 720;
+  @ViewChild("video")
+  public video!: ElementRef;
+  error: any;
+  constructor() {};
+  async ngAfterViewInit() {
+    await this.setupDevices();
   }
-
+  async setupDevices() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true
+        });
+        if (stream) {
+          this.video.nativeElement.srcObject = stream;
+          this.video.nativeElement.play();
+          this.error = null;
+        } else {
+          this.error = "You have no output video device";
+        }
+      } catch (e) {
+        this.error = e;
+      }
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
