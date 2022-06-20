eee7097fd7f541f2bc1b08094efb229e87a82cca
Efrain Estrada <47330403+kevoest@users.noreply.github.com>
2022-06-20 02:42:40 -0600

    log in trie

--- src/app/componentes/log-in/log-in.component.ts
+++ src/app/componentes/log-in/log-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from 'src/services/users.service';
 
 @Component({
   selector: 'app-log-in',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private _registerUser: UsersService) {
+    this.form = this.fb.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+
 
   ngOnInit(): void {
   }
